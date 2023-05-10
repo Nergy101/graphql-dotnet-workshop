5462057d418d945cf42885b6db191aa3bf72d17a
Christian van Dijk <christian.vandijk@akzonobel.com>
2023-05-11 01:01:15 +0200

    dockerized

--- GraphQLFrontend/src/environments/environment.prod.ts
+++ GraphQLFrontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendApiUrl: 'http://localhost:81/graphql/', // docker-compose for demo
-  backendWSUrl: 'ws://localhost:81/graphql/ws' // docker-compose for demo
+  backendApiUrl: 'http://localhost:8080/graphql/', // docker-compose for demo
+  backendWSUrl: 'ws://localhost:8080/graphql/ws' // docker-compose for demo
 };
